bb09dcc9afedc25e43f2d0d4ca35cd24b016136a
nicolas <nicolas@pcOutil.MARAIS>
2020-03-30 17:10:41 +0200

    add outon et slide bar

--- data/scripts/test.js
+++ data/scripts/test.js
@@ -1,13 +1,21 @@
+function getVals(event) {
+    // get min an max value
+    var vals = rangeList.map(x => (parseInt(x.value)));
+    min = Math.min(...vals);
+    max = Math.max(...vals);
+    document.querySelector(".max").innerHTML = max;
+    document.querySelector(".min").innerHTML = min;
+}
+
+//initialise oninput listener on every thumb
+var rangeList = Array.from(document.querySelectorAll(".doubleThumb"));
+rangeList.forEach(x => {
+    x.addEventListener('input', getVals, true);
+});
+
 var host = 'ws://' + window.location.hostname + ':81';
 
 var socket = new WebSocket(host);
-
-// Lorsque la connexion est établie.
-socket.onopen = function(event) {
-    if (event.currentTarget.readyState == 1) {
-        console.log("Connexion établie.");
-    }
-};
 
 // Lorsque le serveur envoi un message.
 socket.onerror = function(event) {
@@ -23,4 +31,11 @@ socket.onclose = function(event) {
 socket.onmessage = function(event) {
     console.log("Message:", event.data);
 
+};
+
+// Lorsque la connexion est établie.
+socket.onopen = function(event) {
+    if (event.currentTarget.readyState == 1) {
+        console.log("Connexion établie.");
+    }
 };
