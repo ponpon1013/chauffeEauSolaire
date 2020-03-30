2e1243a300047063f923867604a71fc09e4869b5
nicolas <nicolas@pcOutil.MARAIS>
2020-03-30 11:38:43 +0200

    OTA configuration

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <ESP8266mDNS.h>
 #include <WiFiManager.h> 
 #include <WebSocketsServer.h>
+#include <ArduinoOTA.h>
 #include <FS.h>
 #include "handler.h"
 
@@ -58,11 +59,42 @@ void handleRootSpiffsError() {
 void setup() {
   Serial.begin ( 9600 );
   //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wifiManager;
-    wifiManager.autoConnect("ChauffeEauConnectAP");
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("ChauffeEauConnectAP");
 
-    //si on arrive là c'est qu'on est connecté au reseau
+  //si on arrive là c'est qu'on est connecté au reseau
+
+  //******* OTA ***************
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("ESPTest");
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+
+  //********* Fin OTA ***************
     
 
     if (!SPIFFS.begin())
@@ -88,5 +120,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   server.handleClient();
   webSocket.loop();
+  ArduinoOTA.handle();
   delay(100);
 }
